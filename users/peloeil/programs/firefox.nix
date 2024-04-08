78a8fdd44d9bb28fd68b8cf2dc44e1ac172eb683
Shun Ota <peloeil20@gmail.com>
2024-04-08 18:56:17 +0900

    chore: default search engine to DuckDuckGo #5

--- users/peloeil/programs/firefox.nix
+++ users/peloeil/programs/firefox.nix
@@ -1,5 +1,20 @@
+{ pkgs, ... }:
 {
   programs.firefox = {
     enable = true;
+    profiles.peloeil = {
+      search = {
+        force = true;
+	default = "DuckDuckGo";
+	privateDefault = "DuckDuckGo";
+      };
+      #containers = {
+      #  programs = {
+      #    color = "blue";
+      #    icon = "chill";
+      #    id = 1;
+      #  };
+      #};
+    };
   };
 }
