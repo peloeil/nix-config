b9f0f566135896744f3d8641a5dc23ff01897044
Shun Ota <peloeil20@gmail.com>
2024-04-10 23:44:28 +0900

    chore: add firefox extensions #5

--- users/peloeil/programs/firefox.nix
+++ users/peloeil/programs/firefox.nix
@@ -2,6 +2,20 @@
 {
   programs.firefox = {
     enable = true;
+    policies = {
+      ExtensionSettings = with builtins;
+      let extension = shortId: uuid: {
+        name = uuid;
+	value = {
+	  install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+          installation_mode = "normal_installed";
+	};
+      };
+      in listToAttrs [
+        (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
+        (extension "enhancer-for-youtube" "enhancerforyoutube@maximerf.addons.mozilla.org")
+      ];
+    };
     profiles.peloeil = {
       search = {
         force = true;
