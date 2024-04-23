831115bd181a407b3e93e606db2bf35c8ff47ea7
Shun Ota <peloeil20@gmail.com>
2024-04-24 01:08:44 +0900

    feat: automatically enable ssh-agent

--- users/peloeil/programs/fish.nix
+++ users/peloeil/programs/fish.nix
@@ -1,5 +1,16 @@
 {
   programs.fish = {
     enable = true;
+    interactiveShellInit = ''
+      set SSH_AGENT_FILE "$HOME/.ssh/ssh_agent"
+      test -f $SSH_AGENT_FILE; and source $SSH_AGENT_FILE > /dev/null 2>&1
+      if not ssh-add -l > /dev/null 2>&1
+        if not ps -ef | grep -v grep | grep ssh-agent
+          ssh-agent -c > $SSH_AGENT_FILE 2>&1
+        end
+        source $SSH_AGENT_FILE > /dev/null 2>&1
+        find $HOME/.ssh -name id_ed25519 | xargs ssh-add
+      end
+    '';
   };
 }
