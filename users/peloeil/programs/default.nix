7f13f09325e090b5e4c72520546864ed3314a64f
Shun Ota <peloeil20@gmail.com>
2024-05-31 20:58:41 +0900

    feat: add waybar for wayland

--- users/peloeil/programs/default.nix
+++ users/peloeil/programs/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ../../../modules/home-manager/alacritty.nix
     ../../../modules/home-manager/kitty.nix
+    ../../../modules/home-manager/waybar.nix
     ./git.nix
     ./firefox.nix
     ./wezterm.nix
