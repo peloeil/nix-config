df7de9c4a769e4c07c64d97a7ccb152c3d983776
Shun Ota <peloeil20@gmail.com>
2024-08-01 03:17:19 +0900

    chore(git): add an alias

--- home-manager/cli/git.nix
+++ home-manager/cli/git.nix
@@ -7,5 +7,10 @@
       init.defaultBranch = "main";
     };
     ignores = ["*.swp"];
+    aliases = {
+      "logs" = ''
+        !f(){ git log --graph --all --abbrev-commit --decorate --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n""           %C(white)%s%C(reset) %C(dim white)- %an%C(reset)"; };f
+      '';
+    };
   };
 }
