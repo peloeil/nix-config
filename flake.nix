fb013cf6db004a347b0f86e1cd1a18435e42f8cd
Shun Ota <peloeil20@gmail.com>
2024-09-01 16:23:56 +0900

    feat: add hlchunk.nvim

--- flake.nix
+++ flake.nix
@@ -15,6 +15,10 @@
       url = "github:stevearc/oil.nvim";
       flake = false;
     };
+    hlchunk_nvim = {
+      url = "github:shellRaining/hlchunk.nvim";
+      flake = false;
+    };
   };
 
   outputs =
