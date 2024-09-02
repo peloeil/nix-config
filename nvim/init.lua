96daae5148861b50053979f87963034a1cc2e14e
Shun Ota <peloeil20@gmail.com>
2024-09-03 02:30:16 +0900

    feat: add my keymap and options

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,11 @@ vim.loader.enable()
 local lazypath = "@lazy_nvim@"
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 require("lazy").setup({
     defaults = { lazy = true },
     spec = "plugins",
 })
+require("options")
+require("keymaps")
