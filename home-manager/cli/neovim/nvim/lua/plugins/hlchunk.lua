bb6824ac12135bd623a7e0bf737995dbe33aed74
Shun Ota <peloeil20@gmail.com>
2024-09-04 03:04:53 +0900

    feat: enhance hlchunk.nvim setup
    it seems blank settings do not work

--- home-manager/cli/neovim/nvim/lua/plugins/hlchunk.lua
+++ home-manager/cli/neovim/nvim/lua/plugins/hlchunk.lua
@@ -2,8 +2,26 @@ return {
     name = "hlchunk.nvim",
     dir = "@hlchunk_nvim@",
     event = {
-        "BufReadPre",
-        "BufNewFile",
+        "BufWinEnter",
     },
-    opts = {},
+    opts = {
+        chunk = {
+            enable = true,
+        },
+        indent = {
+            enable = true,
+        },
+        -- It seems blank settings do not work for now.
+        -- reference: https://github.com/shellRaining/hlchunk.nvim/issues/123
+        blank = {
+            enable = true,
+            chars = { " " },
+            style = {
+                { bg = "#434437" },
+                { bg = "#2f4440" },
+                { bg = "#433054" },
+                { bg = "#284251" },
+            },
+        },
+    },
 }
