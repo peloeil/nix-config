80c3a72386fcba2c75652565c41d208871d65159
Shun Ota <peloeil20@gmail.com>
2024-09-09 14:26:19 +0900

    chore: alpha.nvim as a greeter

--- home-manager/cli/neovim/nvim/lua/plugins/telescope-frecency.lua
+++ home-manager/cli/neovim/nvim/lua/plugins/telescope-frecency.lua
@@ -3,7 +3,7 @@ return {
     dir = "@telescope_frecency_nvim@",
     keys = {
         {
-            "<leader>fc",
+            "<leader>fr",
             function()
                 require("telescope").extensions.frecency.frecency(
                     require("telescope.themes").get_dropdown({ prompt_title = "Frequently Used" })
@@ -13,25 +13,10 @@ return {
             desc = "frecency search",
         },
     },
-    event = function(_, _)
-        local argc = vim.fn.argc()
-        if argc == 0 then
-            return { "VimEnter" }
-        end
-        return {}
-    end,
     dependencies = {
         { name = "telescope.nvim", dir = "@telescope_nvim@" },
     },
     config = function(_, _)
         require("telescope").load_extension("frecency")
-        vim.api.nvim_create_autocmd("UIEnter", {
-            group = vim.api.nvim_create_augroup("__start_frecency", {}),
-            callback = function()
-                require("telescope").extensions.frecency.frecency(
-                    require("telescope.themes").get_dropdown({ prompt_title = "Frequently Used" })
-                )
-            end,
-        })
     end,
 }
