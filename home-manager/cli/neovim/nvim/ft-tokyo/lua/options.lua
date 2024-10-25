d37fe3b7d95700423084fca130acfd9c187315ab
Shun Ota <peloeil20@gmail.com>
2024-10-26 02:49:28 +0900

    chore: visible tabs and spaces for 42tokyo

--- home-manager/cli/neovim/nvim/ft-tokyo/lua/options.lua
+++ home-manager/cli/neovim/nvim/ft-tokyo/lua/options.lua
@@ -14,7 +14,7 @@ vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 vim.opt.tabstop = 4
 vim.opt.list = true
-vim.opt.listchars = { tab = "<->" }
+vim.opt.listchars = { tab = "<->", trail = "-" }
 
 -- enconding
 vim.opt.encoding = "utf-8"
